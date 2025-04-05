f1f5b2b0743aec29f205d6b56de40c726254e71c
Darshan Pawar <darshanpawar47@gmail.com>
2025-04-05 20:03:43 +0530

    Add CI/CD pipeline and Dockerfile

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 
         frameworks: ['jasmine'],
 
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadless'],
 
         plugins: [
             'karma-chrome-launcher',
